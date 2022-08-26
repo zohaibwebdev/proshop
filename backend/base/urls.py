8ec6afca6b45d0c8fd73fe0dbc7048e1008cdbf5
zohaibwebdev <zohaibfas@gmail.com>
2022-08-26 21:15:05 +0500

    user authentication

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.getRoutes, name='routes'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>/', views.getProduct, name='product'),
 ]
